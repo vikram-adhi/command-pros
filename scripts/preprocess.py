52ba2a521b7dfaa7b7cd3ca2c24c9c2774f86fe0
Nithishkumar K R <nithishkumarkr@gmail.com>
2023-11-14 20:25:37 +0530

    TechCon: Added preprocessing code
    modified and fixed bugs

---
+++ scripts/preprocess.py
@@ -0,0 +1,52 @@
+import argparse
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import string
+import os
+import csv
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+custom_stop_words = ["command", "displays", "purpose", "description"]
+stop_words = set(stopwords.words('english') + custom_stop_words)
+
+def preprocess_text(description):
+    description = description.lower()
+
+    tokens = word_tokenize(description)
+
+    tokens = [token for token in tokens if token.isalnum()]
+    tokens = [token for token in tokens if token not in stop_words]
+
+    # Joining tokens back into a string
+    preprocessed_description = ' '.join(tokens)
+
+    return preprocessed_description
+
+
+def main():
+    # Set up the command-line argument parser
+    parser = argparse.ArgumentParser(description="input your file data path for the preprocessed output")
+    parser.add_argument("folder_path", help="Input your dataset folder path")
+    args = parser.parse_args()
+    input_path = os.path.join(args.folder_path, "command_desc.csv")
+    output_path = os.path.join(args.folder_path, "command_desc_preprocessed.csv")
+    
+    df = pd.read_csv(input_path)
+
+    df['Preprocessed_Description'] = df['Description'].apply(preprocess_text)
+    # Save the updated DataFrame to the same CSV file
+    
+    selected_columns = ['Command', 'Link', 'Description', 'Preprocessed_Description']
+
+    df_selected = df[selected_columns]
+    df_selected.to_csv(output_path, index=False)
+
+    print(df_selected.isna().sum())
+    print("Preprocessing complete!")
+if __name__ == "__main__":
+    main()
--- scripts/word_cloud.py
+++ scripts/word_cloud.py
@@ -6,25 +6,15 @@ import csv
 
 # Step 1: Load CSV Data
 # Replace 'data.csv' with your actual CSV file name
-file_path = '../dataset/AOS10/command_desc.csv'
+file_path = '../dataset/AOS8/command_desc.csv'
 
-with open(file_path, 'r', encoding='utf-8') as file:
-    # Create a CSV reader with a custom delimiter (e.g., semicolon)
-    csv_reader = csv.reader(file, delimiter='')
-
-    # Read the CSV data into a list of rows
-    rows = list(csv_reader)
-
-# Convert the list of rows into a DataFrame
-df = pd.DataFrame(rows, columns=['Command', 'Link', 'Description'])
-
-print(df)
-# Replace 'Column3' with the actual column name containing text data
+df = pd.read_csv(file_path)
 text_data = df['Description']
 
 # Step 2: Clean and Prepare Text Data
 text_data = text_data.apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)).lower())
 
+print(text_data)
 # Step 3: Generate Word Cloud
 all_text = ' '.join(text_data)
 wordcloud = WordCloud(width=800, height=400, max_words=200, background_color='white').generate(all_text)
