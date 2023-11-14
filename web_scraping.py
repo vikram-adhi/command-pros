99fc7109ff5f52c0ef389f7a9d7a57c902f68e88
Nithishkumar K R <nithishkumarkr@gmail.com>
2023-11-14 16:32:39 +0530

    TechCon: Improved code in a generic way
    added args

--- web_scraping.py
+++ web_scraping.py
@@ -1,9 +1,12 @@
+import argparse
+import os
+import time
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import csv
+
 my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 
 options = webdriver.ChromeOptions()
@@ -14,9 +17,9 @@ options.add_argument('--disable-gpu')
 options.add_argument(f"--user-agent={my_user_agent}")
 
 #get commands and respective links from the website and save it to csv
-def scrape_data():
+def scrape_data(web_path, command_path):
     wd = webdriver.Chrome(options=options)
-    wd.get("https://www.arubanetworks.com/techdocs/CLI-Bank/Content/landing-pages/aos10-home.htm")
+    wd.get(web_path)
 
     try:
         # Wait for the dynamic content to load
@@ -29,35 +32,35 @@ def scrape_data():
 
         # Extract the content
         all_elements = wd.find_elements(By.CLASS_NAME, 'tree-node-leaf')
-
-        elements = [element for element in all_elements if 'show' in element.text]
         # Create a CSV file and write header
-        with open('output.csv', 'w', newline='', encoding='utf-8') as csvfile:
+                                    
+        with open(command_path, 'w+', newline='', encoding='utf-8') as csvfile:
             fieldnames = ['Command', 'Link']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             writer.writeheader()
-            for element in elements:
+            for element in all_elements:
                 command_text = element.text
-                link = element.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                if(command_text!=''):
+                    link = element.find_element(By.TAG_NAME, 'a').get_attribute('href')
 
-                writer.writerow({'Command': command_text, 'Link': link})
+                    writer.writerow({'Command': command_text, 'Link': link})
 
     finally:
         wd.quit()
 
 #Navitage to all links from csv and collect the Description and save it to csv
-def update_description():
+def update_description(command_path, embeddings_path):
     wd = webdriver.Chrome(options=options)
 
     try:
         # Read commands and links from the CSV file
-        with open('output.csv', 'r', encoding='utf-8') as csvfile:
+        with open(command_path, 'r', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             rows = list(reader)
 
             # Create a new CSV file to store the updated information
-            with open('train_data.csv', 'w', newline='', encoding='utf-8') as output_csvfile:
+            with open(embeddings_path, 'w', newline='', encoding='utf-8') as output_csvfile:
                 fieldnames = ['Command', 'Link', 'Description']
                 writer = csv.DictWriter(output_csvfile, fieldnames=fieldnames)
                 writer.writeheader()
@@ -83,6 +86,18 @@ def update_description():
     finally:
         wd.quit()
 
+
+def main():
+    # Set up the command-line argument parser
+    parser = argparse.ArgumentParser(description="input your Aruba CLI command website and the output path")
+    parser.add_argument("website", help="Input your website URL of Aruba CLI")
+    parser.add_argument("output_path", help="output folder path save the scrapped data along with embeddings")
+    args = parser.parse_args()
+    command_path = os.path.join(args.output_path, "command.csv")
+    embeddings_path = os.path.join(args.output_path, "command_desc.csv")
+
+    scrape_data(args.website, command_path)
+    update_description(command_path, embeddings_path)
+
 if __name__ == "__main__":
-    scrape_data()
-    update_description()
+    main()
